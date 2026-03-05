af39c92b08e6cfce21178c0e1bfd7e39acdfbe44
Yugeun Song <yugeunsong2002@gmail.com>
2026-03-05 23:00:22 +0900

    feat: fix and clean-up ThreadSpawn example code

--- Thread/ThreadSpawn/main.cpp
+++ Thread/ThreadSpawn/main.cpp
@@ -5,101 +5,130 @@
 
 #pragma comment(lib, "Dbghelp.lib")
 
-#define MAX_STACK_TRACE_HISTORY 512
-
-#define ANSI_COLOR_RED "\x1b[31m"
-#define ANSI_COLOR_RESET "\x1b[0m"
-
 CRITICAL_SECTION g_criticalSection;
 
-PVOID g_pAddressHistory[MAX_STACK_TRACE_HISTORY];
-DWORD g_dwHistoryCount = 0;
-
-[[noreturn]] void HandleErrorAndFailW(const wchar_t* pwszMessage)
+[[noreturn]] void HandleErrorAndFailW(LPCWSTR pwszMessage, DWORD dwErrorCode)
 {
-    DWORD dwLastError = GetLastError();
-    fwprintf(stderr, L"%ls: %lu\n", pwszMessage, dwLastError);
+    LPWSTR pwszSysMsg = nullptr;
+
+    FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL,
+                   dwErrorCode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&pwszSysMsg, 0, NULL);
+
+    if (pwszSysMsg == nullptr)
+    {
+        fwprintf(stderr, L"%ls: %lu\n", pwszMessage, dwErrorCode);
+    }
+    else
+    {
+        fwprintf(stderr, L"%ls: %lu - %ls", pwszMessage, dwErrorCode, pwszSysMsg);
+        LocalFree(pwszSysMsg);
+    }
 
     if (IsDebuggerPresent())
     {
         __debugbreak();
     }
-    else
-    {
-        __fastfail(FAST_FAIL_FATAL_APP_EXIT);
-    }
+
+    ExitProcess(dwErrorCode);
 }
 
-void PrintStackTrace(const char* szThreadLabel)
+void PrintStackTrace(LPCSTR szThreadLabel)
 {
     PVOID pStack[64] = { 0, };
     HANDLE hProcess = GetCurrentProcess();
     USHORT usFrames = CaptureStackBackTrace(0, 64, pStack, NULL);
 
-    SYMBOL_INFO* pSymbol = (SYMBOL_INFO*)malloc(sizeof(SYMBOL_INFO) + 256);
-    if (pSymbol == NULL)
+    SYMBOL_INFO* pSymbol = (SYMBOL_INFO*)calloc(1, sizeof(SYMBOL_INFO) + 256);
+    if (pSymbol == nullptr)
     {
+        fwprintf(stderr, L"calloc failed for SYMBOL_INFO in PrintStackTrace\n");
         return;
     }
+
     pSymbol->SizeOfStruct = sizeof(SYMBOL_INFO);
     pSymbol->MaxNameLen = 255;
 
-    EnterCriticalSection(&g_criticalSection);
+    SIZE_T cbMaxOutput = 32768;
+    char* pszOutput = (char*)calloc(1, cbMaxOutput);
+    if (pszOutput == nullptr)
+    {
+        fwprintf(stderr, L"calloc failed for Output Buffer in PrintStackTrace\n");
+        free(pSymbol);
+        return;
+    }
 
-    printf("\n--- Stack Trace: %s (TID: %lu) ---\n", szThreadLabel, GetCurrentThreadId());
+    SIZE_T cbOffset = 0;
+    SIZE_T cbRemaining = cbMaxOutput;
+    SIZE_T cbWrittenCount = 0;
+
+    cbWrittenCount = snprintf(pszOutput + cbOffset, cbRemaining, "\n--- Stack Trace: %s (TID: %lu) ---\n", szThreadLabel,
+                        GetCurrentThreadId());
+    if (cbWrittenCount > 0 && cbWrittenCount < cbRemaining)
+    {
+        cbOffset += cbWrittenCount;
+        cbRemaining -= cbWrittenCount;
+    }
+
+    EnterCriticalSection(&g_criticalSection);
 
     for (USHORT i = 0; i < usFrames; ++i)
     {
-        int isDuplicate = 0;
-
-        for (int j = 0; j < g_dwHistoryCount; ++j)
+        if (pStack[i] == nullptr || cbRemaining == 0)
         {
-            if (g_pAddressHistory[j] == pStack[i])
-            {
-                isDuplicate = 1;
-                break;
-            }
+            continue;
         }
 
         DWORD64 dw64Displacement = 0;
         if (SymFromAddr(hProcess, (DWORD64)pStack[i], &dw64Displacement, pSymbol))
         {
-            if (isDuplicate)
-                printf(ANSI_COLOR_RED);
-
-            printf("[%02u] %-30s (0x%llX)\n", i, pSymbol->Name, pSymbol->Address);
-
-            if (isDuplicate)
-                printf(ANSI_COLOR_RESET);
+            cbWrittenCount = snprintf(pszOutput + cbOffset, cbRemaining, "[%02u] %-30s (0x%llX)\n", i + 1, pSymbol->Name,
+                                pSymbol->Address);
         }
         else
         {
-            printf("[%02u] Unknown Symbol (0x%p)\n", i, pStack[i]);
+            DWORD dwError = GetLastError();
+            cbWrittenCount = snprintf(pszOutput + cbOffset, cbRemaining, "[%02u] Unknown Symbol (0x%p) - Error: %lu\n", i + 1,
+                                pStack[i], dwError);
         }
 
-        if (!isDuplicate && g_historyCount < MAX_STACK_TRACE_HISTORY)
+        if (cbWrittenCount > 0 && cbWrittenCount < cbRemaining)
         {
-            g_pAddressHistory[g_dwHistoryCount] = pStack[i];
-            ++g_dwHistoryCount;
+            cbOffset += cbWrittenCount;
+            cbRemaining -= cbWrittenCount;
+        }
+        else
+        {
+            cbRemaining = 0;
         }
     }
-    printf("--------------------------------------------------\n");
 
     LeaveCriticalSection(&g_criticalSection);
+
+    if (cbRemaining > 0)
+    {
+        snprintf(pszOutput + cbOffset, cbRemaining, "--------------------------------------------------\n");
+    }
+
+    printf("%s", pszOutput);
+
+    free(pszOutput);
+    pszOutput = nullptr;
+
     free(pSymbol);
+    pSymbol = nullptr;
 }
 
 DWORD WINAPI Win32Worker(LPVOID lpParam)
 {
     DWORD dwThreadExitCode = 13;
-    PrintStackTrace("Win32Worker");
+    PrintStackTrace("Win32Worker (CreateThread)");
     return dwThreadExitCode;
 }
 
-unsigned int __stdcall CrtWorker(PVOID pParam)
+UINT __stdcall CrtWorker(PVOID pParam)
 {
-    DWORD dwThreadExitCode = 27;
-    PrintStackTrace("CrtWorker");
+    UINT dwThreadExitCode = 27;
+    PrintStackTrace("CrtWorker (_beginthreadex)");
     return dwThreadExitCode;
 }
 
@@ -110,45 +139,47 @@ int main(void)
     SymSetOptions(SYMOPT_DEFERRED_LOADS | SYMOPT_UNDNAME);
     if (!SymInitialize(GetCurrentProcess(), NULL, TRUE))
     {
-        HandleErrorAndFailW(L"SymInitialize failed");
+        HandleErrorAndFailW(L"SymInitialize failed", GetLastError());
     }
 
-    HANDLE hThreads[2] = { NULL, NULL };
+    HANDLE hThreads[2] = { nullptr, nullptr };
     DWORD dwExitCodes[2] = { 0, 0 };
-    const wchar_t* pwszThreadNames[2] = { L"Win32 Thread", L"CRT Thread" };
+    LPCWSTR pwszThreadNames[2] = { L"Win32 Thread", L"CRT Thread" };
 
     hThreads[0] = CreateThread(NULL, 0, Win32Worker, NULL, 0, NULL);
-    if (hThreads[0] == NULL)
+    if (hThreads[0] == nullptr)
     {
-        HandleErrorAndFailW(L"CreateThread failed");
+        HandleErrorAndFailW(L"CreateThread failed", GetLastError());
     }
 
     hThreads[1] = (HANDLE)_beginthreadex(NULL, 0, CrtWorker, NULL, 0, NULL);
-    if (hThreads[1] == NULL)
+    if (hThreads[1] == nullptr)
     {
-        HandleErrorAndFailW(L"_beginthreadex failed");
+        ULONG nDosErrno = 0;
+        _get_doserrno(&nDosErrno);
+        HandleErrorAndFailW(L"_beginthreadex failed", (DWORD)nDosErrno);
     }
 
-    for (int i = 0; i < 2; ++i)
+    for (SIZE_T i = 0; i < 2; ++i)
     {
         DWORD dwWaitResult = WaitForSingleObject(hThreads[i], INFINITE);
 
         if (dwWaitResult == WAIT_FAILED)
         {
             fwprintf(stderr, L"Wait failed for: %ls\n", pwszThreadNames[i]);
-            HandleErrorAndFailW(L"WaitForSingleObject failed");
+            HandleErrorAndFailW(L"WaitForSingleObject failed", GetLastError());
         }
 
         if (!GetExitCodeThread(hThreads[i], &dwExitCodes[i]))
         {
             fwprintf(stderr, L"Failed to get exit code for: %ls\n", pwszThreadNames[i]);
-            HandleErrorAndFailW(L"GetExitCodeThread failed");
+            HandleErrorAndFailW(L"GetExitCodeThread failed", GetLastError());
         }
     }
 
     printf("\n"
            "============================================\n"
-           "           Thread Execution Results         \n"
+           "            Thread Execution Results        \n"
            "============================================\n"
            "    Win32 API Exit Code       : %-10lu\n"
            "    CRT Wrapper Exit Code     : %-10lu\n"
@@ -157,15 +188,15 @@ int main(void)
 
     if (!CloseHandle(hThreads[0]))
     {
-        HandleErrorAndFailW(L"CloseHandle failed for Win32 thread");
+        fwprintf(stderr, L"CloseHandle failed for Win32 thread: %lu\n", GetLastError());
     }
-    hThreads[0] = NULL;
+    hThreads[0] = nullptr;
 
     if (!CloseHandle(hThreads[1]))
     {
-        HandleErrorAndFailW(L"CloseHandle failed for CRT thread");
+        fwprintf(stderr, L"CloseHandle failed for CRT thread: %lu\n", GetLastError());
     }
-    hThreads[1] = NULL;
+    hThreads[1] = nullptr;
 
     SymCleanup(GetCurrentProcess());
 
